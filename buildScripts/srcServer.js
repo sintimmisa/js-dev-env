b200a90668f7df988698c3765827c047e9752b1e
sintimmisa <sintim.misa@gmail.com>
2020-07-01 20:53:05 +0000

    Setup Express as webserver

---
+++ buildScripts/srcServer.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const open = require('open');
+
+//set PORT
+const PORT = 4000;
+// Create an Instance of express and set to app
+const app = express();
+
+//tell express which routes to handle
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../src/index.html'));
+});
+
+
+
+
+app.listen(PORT, (err) => {
+    if (err) {
+        console.log(err);
+    }
+    else {
+        open('http://localhost:' + PORT);
+    }
+});
